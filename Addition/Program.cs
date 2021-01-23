1019f9ad16058b2c30b5641f4a63ee4a1ab075cc
MrSohux <55574445+mrsohux@users.noreply.github.com>
2021-01-23 20:46:45 +0530

    Add files via upload

---
+++ Addition/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Addition
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            main m = new main();
+            Console.WriteLine("Ente two numbers");
+            int a=Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            int ans = m.add(a, b);
+
+            Console.WriteLine($"addition of{a} + {b} is {ans}");
+        }
+    }
+}
---
+++ Addition/main.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Addition
+{
+    class main
+    {
+        public int add(int a,int b)
+        {
+            return a + b;
+        }
+    }
+}
